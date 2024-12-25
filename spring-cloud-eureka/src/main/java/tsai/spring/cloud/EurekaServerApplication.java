8ae5005c1a5e7b68f4cd482078f6b6da015ac6fc
caixibei <18851037162@139.com>
2024-12-25 15:42:03 +0800

    添加 eureka 配置，参数可选配置

--- spring-cloud-eureka/src/main/java/tsai/spring/cloud/EurekaServerApplication.java
+++ spring-cloud-eureka/src/main/java/tsai/spring/cloud/EurekaServerApplication.java
@@ -15,6 +15,14 @@ public class EurekaServerApplication {
         ApplicationContext context = SpringApplication.run(EurekaServerApplication.class, args);
         Environment environment = context.getEnvironment();
         String port = environment.getProperty("server.port");
-        logger.info("");
+        String fetchRegistry = environment.getProperty("eureka.client.fetch-registry");
+        String registryWithEureka = environment.getProperty("eureka.client.register-with-eureka");
+        String defaultZone = environment.getProperty("eureka.client.service-url.defaultZone");
+        logger.info("===================注册中心运行环境信息===================");
+        logger.info("服务端口：{}",port);
+        logger.info("是否从注册中心拉取服务：{}", fetchRegistry);
+        logger.info("是否注册到注册中心：{}", registryWithEureka);
+        logger.info("注册中心集群地址：{}", defaultZone);
+        logger.info("=======================================================");
     }
 }
