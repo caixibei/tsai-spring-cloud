37d47ed458f18f0b856a398641f23317c4bffc02
caixibei <18851037162@139.com>
2024-12-25 17:42:20 +0800

    ip

--- spring-cloud-eureka/src/main/java/tsai/spring/cloud/EurekaServerApplication.java
+++ spring-cloud-eureka/src/main/java/tsai/spring/cloud/EurekaServerApplication.java
@@ -19,12 +19,14 @@ public class EurekaServerApplication {
         String registryWithEureka = environment.getProperty("eureka.client.register-with-eureka");
         String defaultZone = environment.getProperty("eureka.client.service-url.defaultZone");
         String hostname = environment.getProperty("eureka.instance.hostname");
+        String preferIpAddress = environment.getProperty("eureka.instance.prefer-ip-address");
         logger.info("===================注册中心运行环境信息===================");
         logger.info("服务端口：{}",port);
         logger.info("实例地址：{}",hostname);
         logger.info("是否从注册中心拉取服务：{}", fetchRegistry);
         logger.info("是否注册到注册中心：{}", registryWithEureka);
         logger.info("注册中心集群地址：{}", defaultZone);
+        logger.info("是否优先使用服务器的IP地址：{}", preferIpAddress);
         logger.info("=======================================================");
     }
 }
