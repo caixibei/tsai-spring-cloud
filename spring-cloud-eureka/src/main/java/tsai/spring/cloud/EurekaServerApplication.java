76619597b83de653a0c881595a05ffdfbb93b169
caixibei <18851037162@139.com>
2024-12-25 17:10:25 +0800

    去除端口暴露

--- spring-cloud-eureka/src/main/java/tsai/spring/cloud/EurekaServerApplication.java
+++ spring-cloud-eureka/src/main/java/tsai/spring/cloud/EurekaServerApplication.java
@@ -18,8 +18,10 @@ public class EurekaServerApplication {
         String fetchRegistry = environment.getProperty("eureka.client.fetch-registry");
         String registryWithEureka = environment.getProperty("eureka.client.register-with-eureka");
         String defaultZone = environment.getProperty("eureka.client.service-url.defaultZone");
+        String hostname = environment.getProperty("eureka.instance.hostname");
         logger.info("===================注册中心运行环境信息===================");
         logger.info("服务端口：{}",port);
+        logger.info("实例地址：{}",hostname);
         logger.info("是否从注册中心拉取服务：{}", fetchRegistry);
         logger.info("是否注册到注册中心：{}", registryWithEureka);
         logger.info("注册中心集群地址：{}", defaultZone);
