9c7fcf8fba1f651fb547dfd1d385e41883e8d350
caixibei <18851037162@139.com>
2025-01-07 16:05:28 +0800

    添加登录页面

---
+++ spring-cloud-gateway/src/main/resources/static/common/js/base.js
@@ -0,0 +1,10 @@
+document.write('<link rel="icon" href="assets/image/favicon.ico" type="image/x-icon">')
+document.write('<link rel="stylesheet" href="common/css/element-plus.css"/>')
+document.write('<link rel="stylesheet" href="common/iconfont/iconfont.css"/>')
+document.write('<script src="iconfont/iconfont.js"></script>')
+document.write('<script src="common/js/vue3.js"></script>')
+document.write('<script src="common/js/element-plus.js"></script>')
+document.write('<script src="common/js/vue-router.js"></script>')
+document.write('<script src="common/js/axios.js"></script>')
+document.write('<script src="common/js/request.js"></script>')
+
