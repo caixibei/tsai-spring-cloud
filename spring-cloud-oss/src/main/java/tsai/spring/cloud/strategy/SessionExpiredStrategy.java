b901744ba38eb86c3b7c348cbaf8b5625171c064
caixibei <18851037162@139.com>
2025-02-05 14:13:08 +0800

    oss单点登录添加排他登录

--- spring-cloud-oss/src/main/java/tsai/spring/cloud/strategy/SessionExpiredStrategy.java
+++ spring-cloud-oss/src/main/java/tsai/spring/cloud/strategy/SessionExpiredStrategy.java
@@ -1,31 +1,15 @@
 package tsai.spring.cloud.strategy;
-import com.tsaiframework.boot.result.ResponseResult;
-import org.springframework.security.web.session.InvalidSessionStrategy;
+import org.springframework.security.web.session.SessionInformationExpiredEvent;
+import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 import org.springframework.stereotype.Component;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @Component
-public class SessionExpiredStrategy implements InvalidSessionStrategy {
-
+public class SessionExpiredStrategy implements SessionInformationExpiredStrategy {
     @Override
-    public void onInvalidSessionDetected(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        cancelCookie(request, response);
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(ResponseResult.error("您的账户在别处登录，当前登录失效！").getMessage());
-    }
-
-    protected void cancelCookie(HttpServletRequest request, HttpServletResponse response) {
-        Cookie cookie = new Cookie("JSESSIONID", null);
-        cookie.setMaxAge(0);
-        cookie.setPath(getCookiePath(request));
-        response.addCookie(cookie);
-    }
-
-    protected String getCookiePath(HttpServletRequest request) {
-        String contextPath = request.getContextPath();
-        return !contextPath.isEmpty() ? contextPath : "/";
+    public void onExpiredSessionDetected(SessionInformationExpiredEvent event) throws IOException {
+        HttpServletResponse response = event.getResponse();
+        response.getWriter().print("您的账号在别处登录，当前登录已失效！");
+        response.flushBuffer();
     }
 }
