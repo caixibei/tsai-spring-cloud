4bdd8325c33b5349e84646d81b5ce5a8ca57c8cb
caixibei <18851037162@139.com>
2025-02-06 15:21:56 +0800

    优化登录逻辑

--- spring-cloud-sso/src/main/java/tsai/spring/cloud/strategy/SessionExpiredStrategy.java
+++ spring-cloud-sso/src/main/java/tsai/spring/cloud/strategy/SessionExpiredStrategy.java
@@ -1,4 +1,8 @@
 package tsai.spring.cloud.strategy;
+import cn.hutool.http.HttpStatus;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.tsaiframework.boot.constant.WarningsConstants;
 import org.springframework.security.web.session.SessionInformationExpiredEvent;
 import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 import org.springframework.stereotype.Component;
@@ -9,13 +13,30 @@ import java.io.IOException;
  * @author tsai
  */
 @Component
+@SuppressWarnings(WarningsConstants.DUPLICATES)
 public class SessionExpiredStrategy implements SessionInformationExpiredStrategy {
+
     @Override
     public void onExpiredSessionDetected(SessionInformationExpiredEvent event) throws IOException {
-        HttpServletResponse response = event.getResponse();
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        response.setContentType("application/json; charset=UTF-8");
-        response.getWriter().print("{\"status\":\"403\",\"message\":\"您的账号在别处登录，当前登录失效！\"}");
+        HttpServletResponse response = getHttpServletResponse(event);
+        JSONObject object = new JSONObject();
+        object.putOnce("code", HttpStatus.HTTP_FORBIDDEN);
+        object.putOnce("success", false);
+        object.putOnce("timestamp", System.currentTimeMillis());
+        object.putOnce("message", "您的账号在别处登录，当前登录失效！");
+        response.getWriter().write(JSONUtil.toJsonStr(object));
         response.flushBuffer();
+    }
+
+    protected static HttpServletResponse getHttpServletResponse(SessionInformationExpiredEvent event) {
+        HttpServletResponse response = event.getResponse();
+        response.reset();
+        response.setContentType("application/json;charset=utf-8");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        return response;
     }
 }
