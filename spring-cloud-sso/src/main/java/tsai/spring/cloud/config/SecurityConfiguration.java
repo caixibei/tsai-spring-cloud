a8c26e5d791afb7f945ee6187b995f8420381426
caixibei <18851037162@139.com>
2025-02-09 10:46:16 +0800

    修改授权服务器配置

--- spring-cloud-sso/src/main/java/tsai/spring/cloud/config/SecurityConfiguration.java
+++ spring-cloud-sso/src/main/java/tsai/spring/cloud/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,55 +39,55 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 使用无状态的登录方式
-        http.cors().disable()
-                .csrf().disable()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .logout().disable()
-                .authorizeRequests()
-                .antMatchers("/oauth/**","/sso/lineCaptcha")
-                .permitAll()
-                .anyRequest().authenticated();
+        // http.cors().disable()
+        //         .csrf().disable()
+        //         .formLogin().disable()
+        //         .httpBasic().disable()
+        //         .logout().disable()
+        //         .authorizeRequests()
+        //         .antMatchers("/oauth/**","/sso/lineCaptcha")
+        //         .permitAll()
+        //         .anyRequest().authenticated();
         // 使用有状态的会话管理
-        //http.csrf().disable()
-        //    .formLogin()
-        //    // 自定义的登录页
-        //    .loginPage("/index_v1.html")
-        //    // 必须和前端表单请求地址相同
-        //    .loginProcessingUrl("/login")
-        //    // 登录成功跳转页面
-        //    .successForwardUrl("/dashboard")
-        //    // 登录失败跳转页面
-        //    .failureForwardUrl("/error")
-        //    // 登录失败处理器
-        //    .failureHandler(loginFailureHandler)
-        //    .and()
-        //    .authorizeRequests()
-        //    // 对静态资源、登录请求、获取token请求放行、获取验证码放行
-        //    .antMatchers("/**/*.css", "/**/*.js", "/**/*.jpg",
-        //            "/**/*.png", "/**/*.gif", "/**/*.ico",
-        //            "/**/*.json", "/**/*.ttf", "/**/*.woff",
-        //            "/**/*.woff2", "/index_v1.html","/error.html",
-        //            "/error", "/login", "/oauth/**", "/sso/lineCaptcha")
-        //    .permitAll()
-        //    // 其他所有请求必须通过认证后才能访问
-        //    .anyRequest().authenticated()
-        //    // 异常处理器
-        //    .and().exceptionHandling()
-        //    // 403：无权访问处理器
-        //    .accessDeniedHandler(accessDeniedHandler)
-        //    // 开启 session 会话管理
-        //    .and().sessionManagement()
-        //         // session 创建策略（无状态会话）
-        //         .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-        //         // 应用并发会话策略机制（暂不开启）
-        //         //.sessionAuthenticationStrategy(sessionAuthenticationStrategy())
-        //         // 最多允许登录端数量
-        //         .maximumSessions(1)
-        //         // 多端登录session失效的策略
-        //         .expiredSessionStrategy(sessionExpiredStrategy)
-        //         // 超过最大数量是否阻止新的登录
-        //         .maxSessionsPreventsLogin(false);
+        http.csrf().disable()
+            .formLogin()
+            // 自定义的登录页
+            .loginPage("/index_v1.html")
+            // 必须和前端表单请求地址相同
+            .loginProcessingUrl("/login")
+            // 登录成功跳转页面
+            .successForwardUrl("/dashboard")
+            // 登录失败跳转页面
+            .failureForwardUrl("/error")
+            // 登录失败处理器
+            .failureHandler(loginFailureHandler)
+            .and()
+            .authorizeRequests()
+            // 对静态资源、登录请求、获取token请求放行、获取验证码放行
+            .antMatchers("/**/*.css", "/**/*.js", "/**/*.jpg",
+                    "/**/*.png", "/**/*.gif", "/**/*.ico",
+                    "/**/*.json", "/**/*.ttf", "/**/*.woff",
+                    "/**/*.woff2", "/index_v1.html","/error.html",
+                    "/error", "/login", "/oauth/**", "/sso/lineCaptcha")
+            .permitAll()
+            // 其他所有请求必须通过认证后才能访问
+            .anyRequest().authenticated()
+            // 异常处理器
+            .and().exceptionHandling()
+            // 403：无权访问处理器
+            .accessDeniedHandler(accessDeniedHandler)
+            // 开启 session 会话管理
+            .and().sessionManagement()
+                 // session 创建策略（无状态会话）
+                 .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                 // 应用并发会话策略机制（暂不开启）
+                 //.sessionAuthenticationStrategy(sessionAuthenticationStrategy())
+                 // 最多允许登录端数量
+                 .maximumSessions(1)
+                 // 多端登录session失效的策略
+                 .expiredSessionStrategy(sessionExpiredStrategy)
+                 // 超过最大数量是否阻止新的登录
+                 .maxSessionsPreventsLogin(false);
     }
 
     public SessionRegistryImpl sessionRegistry() {
--- spring-cloud-system/src/main/java/tsai/spring/cloud/core/controller/MenuController.java
+++ spring-cloud-system/src/main/java/tsai/spring/cloud/core/controller/MenuController.java
@@ -1,4 +1,5 @@
 package tsai.spring.cloud.core.controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/system/menu")
 public class MenuController {
 
-    @PostMapping("/getMenu")
+    @GetMapping("/getMenu")
     public String test () {
         return "xxxx";
     }
