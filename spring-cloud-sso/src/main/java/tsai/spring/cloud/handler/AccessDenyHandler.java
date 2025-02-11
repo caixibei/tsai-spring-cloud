eb89cd0d845dc7b73573cfaff94503c40ff8da77
caixibei <18851037162@139.com>
2025-02-11 16:43:31 +0800

    修改了视频背景带来的卡顿问题，不再使用视频背景

--- spring-cloud-sso/src/main/java/tsai/spring/cloud/handler/AccessDenyHandler.java
+++ spring-cloud-sso/src/main/java/tsai/spring/cloud/handler/AccessDenyHandler.java
@@ -10,7 +10,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 /**
  * 无权访问处理器
  * @author tsai
