b4edfd0236834d52beb8fb1970928b115f69fc87
caixibei <18851037162@139.com>
2025-04-05 23:23:41 +0800

    会话过期时效处理

--- spring-cloud-sso/src/main/java/tsai/spring/cloud/config/SecurityConfiguration.java
+++ spring-cloud-sso/src/main/java/tsai/spring/cloud/config/SecurityConfiguration.java
@@ -60,59 +60,58 @@ public class SecurityConfiguration<S extends Session> extends WebSecurityConfigu
     protected void configure(HttpSecurity http) throws Exception {
         // 禁用 csrf
         http.csrf()
-            //.disable()
-            //.headers()
-            //.frameOptions()
-            //.disable()
-            // 禁用 XSS 保护
-            //.xssProtection()
-            //.disable()
-            // 禁用内容类型嗅探保护
-            //.contentTypeOptions()
-            .disable();
+                //.disable()
+                //.headers()
+                //.frameOptions()
+                //.disable()
+                // 禁用 XSS 保护
+                //.xssProtection()
+                //.disable()
+                // 禁用内容类型嗅探保护
+                //.contentTypeOptions()
+                .disable();
         // 认证拦截
         http.authorizeRequests()
-            // 对静态资源、登录请求、获取token请求放行、获取验证码放行
-            .antMatchers("/**/*.css", "/**/*.js", "/**/*.scss",
-                    "/**/*.jpg","/**/*.png", "/**/*.gif", "/**/*.ico",
-                    "/**/*.mp4", "/**/*.webm", "/**/*.json", "/**/*.ttf", "/**/*.woff",
-                    "/**/*.woff2", "/login.html", "/error/403.html",
-                    "/error", "/login", "/oauth/**", "/captcha/**")
-            .permitAll()
-            // 其他所有请求必须通过认证后才能访问
-            .anyRequest().authenticated();
+                // 对静态资源、登录请求、获取token请求放行、获取验证码放行
+                .antMatchers("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.gif", "/**/*.ico",
+                        "/**/*.mp4", "/**/*.webm", "/**/*.json", "/**/*.ttf", "/**/*.woff",
+                        "/**/*.woff2", "/login.html", "/error/403.html",
+                        "/error", "/login", "/oauth/**", "/captcha/**")
+                .permitAll()
+                // 其他所有请求必须通过认证后才能访问
+                .anyRequest().authenticated();
         // 异常处理器
         http.exceptionHandling()
-            // 403：无权访问处理器
-            .accessDeniedHandler(accessDeniedHandler);
+                // 403：无权访问处理器
+                .accessDeniedHandler(accessDeniedHandler);
         // 自定义的认证校验（无状态 JWT 使用）
         // http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         // 开启 session 会话管理（有状态的 session 登录可以使用）
         http.sessionManagement()
-            // session 创建策略（有状态会话）
-            .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-            // 最多允许登录端数量
-            .maximumSessions(1)
-            // 超过最大数量是否阻止新的登录
-            .maxSessionsPreventsLogin(false)
-            // 会话注册表（spring session）
-            .sessionRegistry(springSessionBackedSessionRegistry())
-            // 会话过期策略
-            .expiredSessionStrategy(sessionExpiredStrategy);
+                // session 创建策略（有状态会话）
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                // 最多允许登录端数量
+                .maximumSessions(1)
+                // 超过最大数量是否阻止新的登录
+                .maxSessionsPreventsLogin(false)
+                // 会话注册表（spring session）
+                .sessionRegistry(springSessionBackedSessionRegistry())
+                // 会话过期策略
+                .expiredSessionStrategy(sessionExpiredStrategy);
         // 开启表单登录（有状态的 session 登录可以使用）
         http.formLogin()
-            // 自定义的登录页
-            .loginPage("/login.html")
-            // 必须和前端表单请求地址相同
-            .loginProcessingUrl("/login")
-            // 登录成功跳转页面
-            .successForwardUrl("/home")
-            // 登录失败处理器
-            .failureHandler(loginFailureHandler);
-            // 登录成功处理器
-            // .successHandler(loginSuccessHandler)
-            // 登录失败跳转页面
-            // .failureForwardUrl("/failure");
+                // 自定义的登录页
+                .loginPage("/login.html")
+                // 必须和前端表单请求地址相同
+                .loginProcessingUrl("/login")
+                // 登录成功跳转页面
+                .successForwardUrl("/home")
+                // 登录失败处理器
+                .failureHandler(loginFailureHandler);
+        // 登录成功处理器
+        // .successHandler(loginSuccessHandler)
+        // 登录失败跳转页面
+        // .failureForwardUrl("/failure");
     }
 
     @Bean
